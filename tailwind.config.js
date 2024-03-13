ba73f63773fca716d619e3168835e0e52b9626ee
BoneKing <andym@fortrash.com>
2024-03-12 20:15:47 -0400

    adding more tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}',
+    './lib/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
